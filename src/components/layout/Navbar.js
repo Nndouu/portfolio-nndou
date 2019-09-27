93ad7eaec9dc09ca762115090a6f225f9b90e25d
Nndou <yp.niu@foxmail.com>
2019-09-27 19:24:41 +1000

    Add smooth scroll

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,5 +1,13 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import {
+  Link,
+  DirectLink,
+  Element,
+  Events,
+  animateScroll as scroll,
+  scrollSpy,
+  scroller
+} from "react-scroll";
 
 const Navbar = () => {
   return (
@@ -7,22 +15,50 @@ const Navbar = () => {
       <div className='container'>
         <ul className='navbar-nav m-auto'>
           <li className='nav-item mx-3'>
-            <Link className='nav-link text-primary' to='/'>
+            <Link
+              activeClass='active'
+              to='home'
+              spy={true}
+              smooth={true}
+              duration={500}
+              className='nav-link text-primary'
+            >
               Home
             </Link>
           </li>
           <li className='nav-item mx-3'>
-            <Link className='nav-link text-light' to='/projects'>
+            <Link
+              activeClass='active'
+              to='projects'
+              spy={true}
+              smooth={true}
+              duration={500}
+              className='nav-link text-light'
+            >
               Projects
             </Link>
           </li>
           <li className='nav-item mx-3'>
-            <Link className='nav-link text-light' to='/skills'>
+            <Link
+              activeClass='active'
+              to='skills'
+              spy={true}
+              smooth={true}
+              duration={500}
+              className='nav-link text-light'
+            >
               Skills
             </Link>
           </li>
           <li className='nav-item mx-3'>
-            <Link className='nav-link text-light' to='/contact'>
+            <Link
+              activeClass='active'
+              to='contact'
+              spy={true}
+              smooth={true}
+              duration={500}
+              className='nav-link text-light'
+            >
               Contact
             </Link>
           </li>
