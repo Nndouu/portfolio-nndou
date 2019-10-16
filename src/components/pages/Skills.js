152c37fced98ce919d951170cd4c29ca0e0d7cc0
Nndou <yp.niu@foxmail.com>
2019-10-16 14:36:36 +1000

    Update skills

--- src/components/pages/Skills.js
+++ src/components/pages/Skills.js
@@ -13,13 +13,14 @@ const Skills = () => {
           {/* Tech Skills*/}
           <h3>Tech Skills</h3>
           <p>
-            • Front-End Development: CSS3, Sass, HTML5, AJAX, JavaScript,
-            React.js, React Context API, React Hooks, Bootstrap, Semantic UI,
-            Pug, Responsive design
+            • Front-End Development: React.js, Redux, Redux-Saga, CSS3, Sass,
+            Less, HTML5, Pug(Jade), JavaScript, AJAX, React Context API, React
+            Hooks, Ant.design, Bootstrap, Materialize, Semantic UI, Responsive
+            design, Webpack, ES6
           </p>
           <p>
-            • Back-End Development: Node.js, Express, Mongoose, RESTful API,
-            PHP, CodeIgniter
+            • Back-End Development: Node.js, ExpressJS, Mongoose, RESTful API,
+            KeystoneJS, PHP, CodeIgniter
           </p>
           <p>• Database design and management: MySQL, MongoDB</p>
           <p>
@@ -28,10 +29,10 @@ const Skills = () => {
           </p>
           <p>
             • Machine learning: PyTorch, TensorFlow, Pythonic libraries (NumPy,
-            Panda, Scikit-learn etc), OpenCV
+            Panda, Scikit-learn etc), OpenCV, Weka
           </p>
           <p>• Tool: VSCode, Visual Studio, Eclipse, Jupyter Notebook</p>
-          <p>• Cloud: AWS, Heroku</p>
+          <p>• Cloud: AWS S3, EC2, Heroku, MongoDB Atlas</p>
           {/* UX Skills */}
           <h3>UX Skills</h3>
           <p>• Design Process: Conceptual Design, Interaction Design</p>
@@ -45,7 +46,7 @@ const Skills = () => {
           </p>
           {/*Teamwork Skills */}
           <h3>Teamwork Skills</h3>
-          <p>• Tool: Git, Github, Trello,Bitbucket, Jira</p>
+          <p>• Tool: Git, Github, Trello, Bitbucket, Jira</p>
           <p>• Methodology: Agile, Scrum</p>
         </div>
       </div>
